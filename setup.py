cec884c73ace25b26ae3866268bd53f34dbcc397
jakedahn <jakedahn@gmail.com>
2022-09-25 11:14:49 -0600

    dockerizing for running locally in dev-mode

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1"
+
+setup(
+    name="validatr",
+    version=VERSION,
+    author="Jake Dahn",
+    description="validatr",
+    packages=find_packages(),
+    include_package_data=True,
+)
---
+++ validatr/api/management/commands/wait_for_db.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+import time
+
+
+class Command(BaseCommand):
+    """
+    Django command to pause execution until database is available.
+
+    This is useful for docker-compose
+    """
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("waiting for db ...")
+        time.sleep(5)
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+                self.stdout.write(self.style.SUCCESS("db available"))
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second ...")
+                time.sleep(1)
